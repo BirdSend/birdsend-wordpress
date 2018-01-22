fd5c91092d9520d0dcc7bd3d2c5734638f6571c6
Adi Djohari <adidjohari@gmail.com>
2018-01-22 12:59:27 +0700

    base plugin

---
+++ birdsend-for-wordpress.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Plugin Name: BirdSend for WordPress
+ * Version: 1.0.0
+ * Plugin URI: https://birdsend.co/
+ * Description: Official plugin to integrate BirdSend with WordPress.
+ * Author: XooGuu Team
+ * Author URI: https://birdsend.co/
+ * License: GPL2
+ * Domain Path: /languages/
+ */
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2018 XooGuu, LLC.
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+define( 'BSWP_VERSION', '1.0.0' );
+define( 'BSWP_DB_VERSION', '0.1' );
+
+define( 'BSWP_URL', plugin_dir_url(__FILE__) );
+define( 'BSWP_PATH', plugin_dir_path(__FILE__) );
+define( 'BSWP_BASENAME', plugin_basename( __FILE__ ) );
+
+define( 'BSWP_INC', BSWP_PATH . 'includes/');
+
+define( 'BSWP_JS', BSWP_URL . 'assets/js/' );
+define( 'BSWP_CSS', BSWP_URL . 'assets/css/' );
+define( 'BSWP_IMG', BSWP_URL . 'assets/img/' );
+
+// load files
+require_once( BSWP_INC . 'admin.php' );
---
+++ includes/admin-settings.php
@@ -0,0 +1,31 @@
+<div class="bswp-wrapper">
+	<header>
+		<nav class="grey lighten-5">
+		    <div class="nav-wrapper">
+		      	<a href="https://birdsend.co" target="_blank" class="brand-logo center"><img src="<?php echo BSWP_IMG; ?>logo-sm.png" ></a>
+		      	<!--
+		      	<ul id="nav-mobile" class="right hide-on-med-and-down">
+		        	<li><a href="sass.html">Sass</a></li>
+		        	<li><a href="badges.html">Components</a></li>
+		        	<li><a href="collapsible.html">JavaScript</a></li>
+		      	</ul>
+		      	//-->
+		    </div>
+		</nav>
+	</header>
+	<div class="bswp-container">
+		<div class="row">
+        	<div class="col s12 l12">
+          		<div class="card card-wide">
+            		<div class="card-content">
+            			<span class="badge red lighten-2 white-text">Not Connected</span>
+              			<span class="card-title"><strong>API Settings</strong></span>
+
+              			<div style="margin: 15px 0"><button class="waves-effect waves-light btn orange lighten-2">Connect My BirdSend</button></div>
+              			<p>Click the button above to integrate WordPress with your BirdSend account</p>
+            		</div>
+          		</div><!-- ./card -->
+        	</div><!-- ./col -->
+      	</div><!-- ./row -->
+	</div><!-- ./bswp-container -->
+</div>
