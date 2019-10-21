13fbcbb6002b4670a00ae82402478d1c589e38c9
Siswo <siswo@syntaxq.com>
2019-10-21 10:54:37 +0700

    add subscription cancel action

--- libs/woocommerce.php
+++ libs/woocommerce.php
@@ -5,10 +5,12 @@
  *
  * @return void
  */
-function bswo_post_order( $order_id, $action = 'purchase_product' ) {
+function bswo_post_order( $order_id, $action = 'purchase_product', $items= array()) {
 	$order = wc_get_order( $order_id );
 	$email = $order->get_billing_email();
-	$items = $order->get_items();
+	if ( empty( $items ) ) {
+		$items = $order->get_items();
+	}
 	$ipaddress = getenv( 'HTTP_CLIENT_IP' ) ?:
 				  getenv( 'HTTP_X_FORWARDED_FOR' ) ?:
 				  getenv( 'HTTP_X_FORWARDED' ) ?:
@@ -38,10 +40,12 @@ function bswo_post_order( $order_id, $action = 'purchase_product' ) {
 			$options['products'] = $products;
 			$options['action'] = $action;
 			$options['site_url'] = $site;
+			$options['order_id'] = $order_id;
 		}
 		$post = [
 			'form_params' => [ $options ],
 		];
+		
 		$url = bswp_app_url( 'listener' );
 		$response = $http->request( 'POST', $url, $post );
 
@@ -71,9 +75,22 @@ function refund_success( $order_id ) {
 }
 add_action( 'woocommerce_order_status_refunded', 'refund_success', 1 ); 
 
-// function cancel_success() {
-// 	bswo_post_order($order_id,'cancel');
-// }
+function new_order( $order_id ) {
+ 	bswo_post_order( $order_id, 'abandon' );
+}
+add_action( 'woocommerce_new_order', 'new_order', 1 );
+
+function cancel_subscription( $subscription ) {
+	$orders =  $subscription->get_related_orders();
+	foreach ($orders as $order) {
+		$id = $order;
+	}
+	if ( $subscription->get_status() == "cancelled" ) {
+		$items = $subscription->get_items();
+		bswo_post_order( $id, 'subscription_cancel', $items );
+	}
+}
+add_action( 'woocommerce_subscription_status_updated', 'cancel_subscription', 1 );
 
 // add_action('woocommerce_subscription_payment_complete','cancel_success', 1);
 ?>
