c7b98ea06e8cd7ae2777e877b94e1f5c3a81d8ca
Ahmad Subhan <ahmad.subhan@aol.com>
2021-08-31 09:43:10 +0700

    Updated version

--- birdsend-email-marketing.php
+++ birdsend-email-marketing.php
@@ -28,7 +28,7 @@ Copyright 2018 XooGuu, LLC.
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'BSWP_VERSION', '1.0.10' );
+define( 'BSWP_VERSION', '1.0.11' );
 define( 'BSWP_DB_VERSION', '0.1' );
 
 define( 'BSWP_URL', plugin_dir_url(__FILE__) );
